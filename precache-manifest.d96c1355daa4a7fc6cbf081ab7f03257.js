d7d41b795f0ebda35ec3ffec49e3cd5b56acd5ed
Unknown8991 <rafal.pat@onet.pl>
2021-09-21 16:01:22 +0200

    Updates

---
+++ precache-manifest.d96c1355daa4a7fc6cbf081ab7f03257.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "7524b6dd0d78bf0e1af6",
+    "url": "/MainWebsite/static/css/main.fe5cdbbe.chunk.css"
+  },
+  {
+    "revision": "7524b6dd0d78bf0e1af6",
+    "url": "/MainWebsite/static/js/main.7ce272e6.chunk.js"
+  },
+  {
+    "revision": "c46fc70fc1ca555b6d96",
+    "url": "/MainWebsite/static/js/runtime~main.6cf8e403.js"
+  },
+  {
+    "revision": "4fd313b38b05e50e708f",
+    "url": "/MainWebsite/static/css/2.bcd1ebaf.chunk.css"
+  },
+  {
+    "revision": "4fd313b38b05e50e708f",
+    "url": "/MainWebsite/static/js/2.70fdb7f1.chunk.js"
+  },
+  {
+    "revision": "ad931f297aa414faa42ec6b063d34037",
+    "url": "/MainWebsite/static/media/artist.ad931f29.svg"
+  },
+  {
+    "revision": "aa24918d36e78a1a8fb2ffc7644d7595",
+    "url": "/MainWebsite/static/media/project-management.aa24918d.svg"
+  },
+  {
+    "revision": "69ffd5c9cce9003873a45c5af7c3bfb0",
+    "url": "/MainWebsite/static/media/contact-form.69ffd5c9.svg"
+  },
+  {
+    "revision": "7ad1f30c9e9d1b8f2f6934b423e8626e",
+    "url": "/MainWebsite/static/media/man.7ad1f30c.svg"
+  },
+  {
+    "revision": "a0657d1cac3b20f1e113738056db3c63",
+    "url": "/MainWebsite/static/media/icons8-html-5.a0657d1c.svg"
+  },
+  {
+    "revision": "dce14c443f43902efc375e2b3629b6a4",
+    "url": "/MainWebsite/static/media/icons8-sass.dce14c44.svg"
+  },
+  {
+    "revision": "8ee02715beb101d7d505a9473bfb2754",
+    "url": "/MainWebsite/static/media/icons8-javascript.8ee02715.svg"
+  },
+  {
+    "revision": "dce37cd15211c519dc6328b03055b96a",
+    "url": "/MainWebsite/static/media/icons8-react.dce37cd1.svg"
+  },
+  {
+    "revision": "b00020dde0490b3f0ecb3643f369d99c",
+    "url": "/MainWebsite/static/media/icons8-vue-js(1).b00020dd.svg"
+  },
+  {
+    "revision": "5eed2c8cb62e1adfae09b0129a25a1a7",
+    "url": "/MainWebsite/static/media/icons8-git.5eed2c8c.svg"
+  },
+  {
+    "revision": "6d0d97c61c3f3b2f8082490c6ad07fb7",
+    "url": "/MainWebsite/static/media/ig.6d0d97c6.jpg"
+  },
+  {
+    "revision": "0e1f65044830ca1f3d896b7cc4956b5a",
+    "url": "/MainWebsite/static/media/igb.0e1f6504.jpg"
+  },
+  {
+    "revision": "0e3e8c22269f21ab3dbb6086bc0bf91a",
+    "url": "/MainWebsite/static/media/igb2.0e3e8c22.jpg"
+  },
+  {
+    "revision": "600616dd28be45bcc490bc0df5a40e96",
+    "url": "/MainWebsite/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/MainWebsite/precache-manifest.d96c1355daa4a7fc6cbf081ab7f03257.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/MainWebsite/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
